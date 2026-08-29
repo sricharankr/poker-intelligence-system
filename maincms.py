aeed74de3c2de6056176db2e425ec6136446bc66
Sricharan R <srichrkbusiness@gmail.com>
2026-08-29 21:18:27 +0200

    Add signup flow to maincms.py

---
+++ maincms.py
@@ -0,0 +1,12 @@
+print("Welcome to VIEWPOINT!")
+
+
+def signup():
+    username = input("What is your name? ")
+    print(f"Thank you {username}! Click 'start' to begin building your personalized cashflow system.")
+
+signup()
+
+
+
+
